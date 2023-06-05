386652f381378931d70c394b7be391b97c7b6d2f
Eito Takase <eito01178@gmail.com>
2023-06-05 23:18:07 +0900

    リロードは敵(自分の設計が悪い)

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, globalShortcut, screen, Menu } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 import { loadData, saveData } from './utils/save-data';
@@ -113,7 +113,30 @@ function createWindow(): BrowserWindow {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-app.on('ready', () => setTimeout(createWindow, 400));
+app.on('ready', () => {
+  setTimeout(createWindow, 400);
+  // Reloadを防ぐ
+  globalShortcut.register('CommandOrControl+R', () => {});
+  globalShortcut.register('CommandOrControl+Shift+R', () => {});
+
+  const template = [
+    {
+      label: 'Application',
+      submenu: [
+        {
+          label: 'Exit',
+          accelerator: 'CmdOrCtrl+Q',
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
